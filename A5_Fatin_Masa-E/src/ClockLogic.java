b1d5d1a6eb7293a0419ebd2c1df519f1b45733ef
Fatinm <fatinm@live.se>
2015-02-18 09:33:40 +0100

    Endast hunnit skriva ut klasserna enligt digrammet, är de rätt?

---
+++ A5_Fatin_Masa-E/src/ClockLogic.java
@@ -0,0 +1,29 @@
+
+public class ClockLogic implements ClockInterface {
+	private DigitalClockGUI clockGUI;
+	private int alarmHour;
+	private int alarmMinute;
+	
+	public ClockLogic(DigitalClockGUI clockIn){
+	
+	
+	}
+	
+	
+	public void setAlarm(int hours, int minute){
+		
+	}
+	
+	public void clearAlarm(){
+		
+	}
+	
+	
+	
+	@Override
+	public void update(int hours, int minute, int second) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ A5_Fatin_Masa-E/src/ClockThread.java
@@ -0,0 +1,15 @@
+
+public class ClockThread extends Thread {
+
+	private ClockInterface clockInterface;
+
+
+	public ClockThread(ClockInterface ci) {
+
+	}
+
+	public void run(){
+
+	}
+
+}
